07c5476b779a87a1ddaf346a3a00621b1085d98b
estebanrm1 <solsel90@gmail.com>
2023-07-13 12:34:52 -0300

    agregamos las rutas con sus metodos para administrar tareas

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { borrarTarea, crearTarea, obtenerTareas } from "../controllers/tareas.controllers";
 
 const router = Router();
 
+router.route('/tareas').get(obtenerTareas).post(crearTarea);
+router.route('/tareas/:id').delete(borrarTarea)
 
 export default router
