3be339b6ae9973e8754d43e3e155d0d9a53d31ae
estebanrm1 <solsel90@gmail.com>
2023-07-13 12:17:38 -0300

    agregamos la configuacion en el index y conectamos a mongo db

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ import cors from 'cors';
 import morgan from "morgan";
 import path from 'path';
 import 'dotenv/config';
+import './src/database/bdConnection'
+import tareasRouter from './src/routes/tareas.routes'
 
 const app = express()
 
 app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'));
+})
+
+app.use(cors()); //permitir conexiones remotas
+app.use(express.json()); //permite a mi app recibir objetos en formato json en los request
+app.use(morgan('dev')) //muestra en consola info extra de las solicitudes
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use('apitareas', tareasRouter)
